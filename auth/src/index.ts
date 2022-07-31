60e883e8f012618ccb67f998e88daf261d43a674
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-08-01 00:00:11 +0200

    feat: Implement not found error type.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import {signinRouter} from "./routes/signin";
 import {signoutRouter} from "./routes/signout";
 import {signupRouter} from "./routes/signup";
 import {errorHandler} from "./middlewares/error-handler";
+import {NotFoundError} from "./errors/not-found-error";
 
 
 const app = express()
@@ -15,7 +16,13 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+
+app.get('*', () => {
+	throw new NotFoundError()
+})
+
 app.use(errorHandler)
+
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000!!!')
