47f6b632efdd319ce7f99d84704324b88b387462
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-08-01 09:53:21 +0200

    feat: Connect auth service with the MongoDB database.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+
+import 'express-async-errors'
 import {json} from 'body-parser'
 
 import {currentUserRouter} from "./routes/current-user";
@@ -7,6 +9,7 @@ import {signoutRouter} from "./routes/signout";
 import {signupRouter} from "./routes/signup";
 import {errorHandler} from "./middlewares/error-handler";
 import {NotFoundError} from "./errors/not-found-error";
+import mongoose from "mongoose";
 
 
 const app = express()
@@ -23,7 +26,17 @@ app.get('*', () => {
 
 app.use(errorHandler)
 
+const start = async () => {
+	try {
+		console.log('Connecting to MongoDB!')
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+	} catch (err) {
+		console.error(err)
+	}
 
-app.listen(3000, () => {
-	console.log('Listening on port 3000!!!')
-})
+	app.listen(3000, () => {
+		console.log('Listening on port 3000!!!')
+	})
+}
+
+void start()
