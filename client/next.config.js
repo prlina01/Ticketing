71dbaa4bcb7b3c6807acd2177e7d5c9b8cf57d2e
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-08-04 15:45:12 +0200

    feat: Add styling library and finish the configuration.

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  // reactStrictMode: true,
+  // swcMinify: true,
+  //   webpack: function (config, options) {
+  //       options.webpack.poll = 300
+  //       return config
+  //   },
+
+    assetPrefix: "."
 }
 
 module.exports = nextConfig
---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h2>123</h2>
+    </>
+  )
+}
+
+export default Home
