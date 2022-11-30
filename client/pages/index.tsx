c2fa5b19b545ec728637f68d7a87680678d56c33
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-11-30 23:37:23 +0100

    feat: 1) Fix the SSR-kubernetes error. 2) Implement fetching of logged-in user's data on the client and side.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,17 +2,39 @@ import type { NextPage } from 'next'
 import axios from 'axios'
 
 const Home: NextPage = ({currentUser}: any) => {
-  return (
+
+    console.log(currentUser)
+    return (
     <>
-      <h2>{currentUser}</h2>
+      <h2>Landing page</h2>
     </>
   )
 }
 
 Home.getInitialProps = async () => {
-    const response = await axios.get('/api/users/currentuser')
+    if (typeof window === 'undefined') {
+        // we are on the server
+        // requests should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local....
+        const {data} = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.cluster.local/api/users/currentuser',
+            {
+                headers: {
+                    Host: 'ticketing.dev'
+                }
+            }
+        )
 
-    return response.data;
+        return data
+
+    } else {
+        // we are on the browser/client-side!
+        // we come here only when we get redirected to this page from the same website!
+        // requests can be made with a base url of ''
+        const {data} = await axios.get('/api/users/currentuser')
+        return data;
+    }
+
+
 }
 
 
