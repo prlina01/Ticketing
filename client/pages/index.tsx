b1255a79b53c410fe03e46998494e66df61f272d
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-12-02 13:09:12 +0100

    feat: Add a login page.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,13 +3,9 @@ import axios from 'axios'
 import buildClient from "../api/build-client";
 
 const Home: NextPage = ({currentUser}: any) => {
-
-    console.log(currentUser)
-    return (
-    <>
-      <h2>Landing page</h2>
-    </>
-  )
+    return(
+        <h1>{currentUser ? 'You are signed in!' : 'You are not signed in'}</h1>
+    )
 }
 
 Home.getInitialProps = async (context) => {
