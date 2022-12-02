a774e9d71248d47c172bcfb3e3dc37f52316c274
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-12-02 13:01:27 +0100

    feat: Create a function that abstracts the domain choice when doing SSR.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import axios from 'axios'
+import buildClient from "../api/build-client";
 
 const Home: NextPage = ({currentUser}: any) => {
 
@@ -11,29 +12,11 @@ const Home: NextPage = ({currentUser}: any) => {
   )
 }
 
-Home.getInitialProps = async ({req}) => {
-    if (typeof window === 'undefined') {
-        // we are on the server
-        // requests should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local....
-        const {data} = await axios.get(
-            'http://ingress-nginx-controller.ingress-nginx.cluster.local/api/users/currentuser',
-            {
-                // @ts-ignore
-                headers: req.headers
-            }
-        )
+Home.getInitialProps = async (context) => {
+    const client = buildClient(context)
+    const { data } = await client.get('/api/users/currentuser')
 
-        return data
-
-    } else {
-        // we are on the browser/client-side!
-        // we come here only when we get redirected to this page from the same website!
-        // requests can be made with a base url of ''
-        const {data} = await axios.get('/api/users/currentuser')
-        return data;
-    }
-
-
+    return data
 }
 
 
